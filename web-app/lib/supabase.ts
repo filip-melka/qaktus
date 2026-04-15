6052cfd234415df54bb318607039366bed8488be
Filip Melka <melka.filip@proton.me>
2026-04-15 10:52:55 +0200

    fix: remove waitlist

--- web-app/lib/supabase.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export function createServerSupabaseClient() {
-  return createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_API_KEY!);
-}
